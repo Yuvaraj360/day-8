2697b2215c86bd9697aae339fe2ce7749c55e493
Yuvaraj360 <137602148+Yuvaraj360@users.noreply.github.com>
2023-07-20 09:49:51 +0530

    Add files via upload

---
+++ day 8/Main (1).java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.io.*;
+import  java.util.*;
+class UserMainCode
+{
+public int EvenDigitsSum(int inpu1){
+int sum=0;
+while(input1!=0)
+{
+int n=input1%10;
+if(n%2==0)
+sum+=n;
+input1/=10; 
+}
+return sum;
+}
